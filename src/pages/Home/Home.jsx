5d04ee89eed9a94abd5c5f2cf57760034a92bf2a
ozng <ozangurer12@gmail.com>
2022-08-22 13:27:49 +0300

    Redux-Router-Basic setups

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import "./home.css";
+
+function Home() {
+  return (
+    <div>
+      <h1>Movie</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/store/reducers/movie.js
@@ -0,0 +1,14 @@
+const initialState = {
+  selected: {},
+};
+
+const movieReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return {
+        ...state,
+      };
+  }
+};
+
+export default movieReducer;
